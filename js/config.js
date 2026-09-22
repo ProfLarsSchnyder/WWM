3999541ef132bc6c200928b94497b753bd79f513
ProfLarsSchnyder <lars.schnyder1@gmail.com>
2026-09-22 22:15:47 +0200

    Füge Supabase Konfiguration hinzu

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL = "";
+export const SUPABASE_PUBLISHABLE_KEY = "";
+
+// Nur Project URL und Publishable Key eintragen.
+// Niemals einen service_role Key in eine Browser App oder in GitHub schreiben.
